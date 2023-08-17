2e2943c67ed555362e32ac982375e6142eef7df2
Christian <83311493+xatitive@users.noreply.github.com>
2023-08-17 02:15:00 -0400

    Initial Commit

---
+++ ForceTouchSlider-SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ForceTouchSlider-SwiftUI
+//
+//  Created by Christian Norton on 8/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ForceTouchSlider-SwiftUI/ForceTouchSlider_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForceTouchSlider_SwiftUIApp.swift
+//  ForceTouchSlider-SwiftUI
+//
+//  Created by Christian Norton on 8/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ForceTouchSlider_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
